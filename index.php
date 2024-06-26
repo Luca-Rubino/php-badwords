fbe0d8abc66747be26812545d3b8ebe70cfc2bdb
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-26 09:49:29 +0200

    Inserire nel dom stampa lunghezza della nuova stringa generata, correzione stile

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 
 <?php  
-    $titolo = 'php-badwords';
+    $titolo = 'Php-Badwords';
     $testo = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit architecto repellendus fuga ducimus perspiciatis tempore alias sequi placeat cumque, ratione ea aliquid harum. Maiores architecto repellat delectus excepturi, error natus.
     Soluta doloremque maxime consequuntur sunt ex, fuga exercitationem obcaecati iusto tempore, aliquid minima fugiat sint ut delectus nam mollitia enim animi iure sequi ratione! Ducimus vel veritatis distinctio mollitia ipsam.
     Est facere id mollitia placeat nemo numquam officiis! Ipsum explicabo ipsa iste blanditiis exercitationem sed, obcaecati, voluptatem labore dignissimos debitis veritatis! Possimus dolore enim laudantium quae repudiandae explicabo quibusdam sit?
     Facere at nostrum reiciendis, aliquid ducimus blanditiis excepturi aliquam beatae odit minima exercitationem ipsum itaque esse sed! Consequuntur dolor deserunt iusto perspiciatis fuga dolore vitae eveniet aspernatur! Vitae, eius ut.
     Aliquid amet veritatis voluptates dignissimos, nam delectus, cupiditate perferendis et non possimus iusto repellat nihil blanditiis sint ut, quasi id nemo. Molestias sed provident temporibus impedit eveniet eligendi explicabo quisquam.';
-    $filtroCensura = $_GET["filtroCensura"];
+    $filtroCensura = $_GET["filtro-censura"];
     $nuovoTesto = str_replace($filtroCensura, "***", $testo);
 ?>
 
@@ -15,7 +15,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
     <title>php-badwords</title>
 </head>
@@ -33,13 +32,14 @@
     </section>
     <section>
         <form action="./index.php" method="get">
-            <label for="filtroCensura">Cosa vuoi censurare?</label>
-            <input type="text" name="filtroCensura" id="filtroCensura">
+            <label for="filtro-censura">Cosa vuoi censurare?</label>
+            <input type="text" name="filtro-censura" id="filtro-censura">
             <input type="submit" value="Invia">
         </form>
     </section>
     <section>
         <h2>Testo censurato</h2>
+        <h3>Questo paragrafo censurato è lungo: <span><?php echo strlen($nuovoTesto) ?></span> caratteri.</h3>
         <p> <?php echo $nuovoTesto ?> </p>
     </section>
 </header>
