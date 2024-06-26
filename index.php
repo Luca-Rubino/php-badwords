5164f27e0880e1ade49c8b6e0ca3d05086fed1e6
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-26 02:02:33 +0200

    Base scaffolding

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php  
+    $titolo = 'php-badwords'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+<header>
+    <section>
+        <h1>
+            <?php echo $titolo ?>
+        </h1>
+    </section>
+</header>
+
+</body>
+</html>
