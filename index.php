166589d9ec52796f77737aeb785fdade0be5ecfd
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-26 02:07:58 +0200

    Creare variabile con all'interno del testo

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 
 <?php  
-    $titolo = 'php-badwords'; 
+    $titolo = 'php-badwords';
+    $testo = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit architecto repellendus fuga ducimus perspiciatis tempore alias sequi placeat cumque, ratione ea aliquid harum. Maiores architecto repellat delectus excepturi, error natus.
+    Soluta doloremque maxime consequuntur sunt ex, fuga exercitationem obcaecati iusto tempore, aliquid minima fugiat sint ut delectus nam mollitia enim animi iure sequi ratione! Ducimus vel veritatis distinctio mollitia ipsam.
+    Est facere id mollitia placeat nemo numquam officiis! Ipsum explicabo ipsa iste blanditiis exercitationem sed, obcaecati, voluptatem labore dignissimos debitis veritatis! Possimus dolore enim laudantium quae repudiandae explicabo quibusdam sit?
+    Facere at nostrum reiciendis, aliquid ducimus blanditiis excepturi aliquam beatae odit minima exercitationem ipsum itaque esse sed! Consequuntur dolor deserunt iusto perspiciatis fuga dolore vitae eveniet aspernatur! Vitae, eius ut.
+    Aliquid amet veritatis voluptates dignissimos, nam delectus, cupiditate perferendis et non possimus iusto repellat nihil blanditiis sint ut, quasi id nemo. Molestias sed provident temporibus impedit eveniet eligendi explicabo quisquam.';
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +22,9 @@
         <h1>
             <?php echo $titolo ?>
         </h1>
+    </section>
+    <section>
+        <p><?php echo $testo ?></p>
     </section>
 </header>
 
